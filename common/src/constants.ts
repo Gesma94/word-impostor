660f3d77f39151f34bf560adca34a9bc23409de3
Gesma94 <46707463+Gesma94@users.noreply.github.com>
2024-01-12 00:18:39 +0100

    F!! delete common folder

--- common/src/constants.ts
+++
@@ -1,5 +0,0 @@
-export const WS_MESSAGE_EVENT_CREATE_ROOM = 'create-room';
-export const WS_MESSAGE_EVENT_JOIN_ROOM = 'join-room';
-export const WS_MESSAGE_EVENT_START_ROOM = 'start-room';
-export const WS_MESSAGE_EVENT_PLAYER_JOIN = 'player-joined';
-export const WS_MESSAGE_EVENT_ROOM_STARTED = 'room-started';

74ba5177647652484fa46da90cf5667268b2d445
Matteo Gesmundo <46707463+Gesma94@users.noreply.github.com>
2024-06-10 00:30:33 +0200

    fix: use default port

--- apps/server/src/index.ts
+++ apps/server/src/index.ts
@@ -3,6 +3,7 @@ import type { CommonType } from "@word-impostor/common/types";
 import { isNullOrUndefined } from "@word-impostor/common/utils";
 import Fastify from "fastify";
 
+const port = process.env.PORT ? Number(process.env.PORT) : 7717;
 const fastify = Fastify({
   logger: true,
 });
@@ -19,7 +20,7 @@ fastify.get("/env", (_, reply) => {
   reply.send({ env: process.env.SERVER_COMMON_ENV });
 });
 
-fastify.listen({ port: 3000 }, (err, address) => {
+fastify.listen({ port }, (err, address) => {
   fastify.log.debug(`Listening at ${address}`);
   if (err) throw err;
 });

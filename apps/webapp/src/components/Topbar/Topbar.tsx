cbba3161191bcc8d895d681af2977925e2fe2449
Matteo Gesmundo <46707463+Gesma94@users.noreply.github.com>
2024-07-16 00:37:30 +0200

    feat: add support to Storybook

---
+++ apps/webapp/src/components/Topbar/Topbar.tsx
@@ -0,0 +1,10 @@
+export const Topbar: React.FC = () => {
+  return (
+    <nav>
+      <h1>WORD IMPOSTOR</h1>
+      <p>Join room</p>
+      <p>Create room</p>
+      <p>Matteo</p>
+    </nav>
+  );
+};
